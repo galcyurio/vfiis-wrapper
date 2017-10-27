869c8f63d5b165a557ec26cec5ce15ee31ffe61b
galcyurio <galcyurio@gmail.com>
2017-10-28 00:52:09 +0900

    general POGOs added

---
+++ vfiis-wrapper/src/main/groovy/com/github/galcyurio/response/ForecastGribResponse.groovy
@@ -0,0 +1,9 @@
+package com.github.galcyurio.response
+
+/**
+ * 호출 메시지의 검색 조건에 따라 실황 정보를 제공한다.
+ *
+ * @author galcyurio
+ */
+class ForecastGribResponse {
+}
---
+++ vfiis-wrapper/src/main/groovy/com/github/galcyurio/response/ForecastVersionCheckResponse.groovy
@@ -0,0 +1,9 @@
+package com.github.galcyurio.response
+/**
+ * 호출 메시지의 검색 조건에 따라 예보 버전 정보를 제공한다.
+ *
+ * @author galcyurio
+ */
+class ForecastVersionCheckResponse {
+
+}
