be64ae7f7728a0b036f26ec68f3fb844e411da90
galcyurio <galcyurio@gmail.com>
2017-10-28 14:50:12 +0900

    prepare for 0.0.2

--- vfiis-wrapper/src/main/groovy/com/github/galcyurio/core/Operation.groovy
+++ vfiis-wrapper/src/main/groovy/com/github/galcyurio/core/Operation.groovy
@@ -1,6 +1,8 @@
 package com.github.galcyurio.core
 
 /**
+ * 동네 예보 정보 조회 서비스 오퍼레이션(최근 24시간 자료)
+ *
  * @author galcyurio
  */
 enum Operation {
@@ -10,7 +12,7 @@ enum Operation {
      * 예보지점 Y 좌표의 조회 조건으로 자료구분코드, 실황값, 발표일자, 발표시각,
      * 예보지점 X 좌표, 예보지점 Y 좌표의 정보를 조회하는 기능
      */
-    FORECAST_GRIP,
+    FORECAST_GRIB,
 
     /**
      * 동네예보 정보를 조회하기 위해 발표일자, 발표시각, 예보지점 X 좌표,
--- vfiis-wrapper/src/main/groovy/com/github/galcyurio/request/ForecastGribRequest.groovy
+++ vfiis-wrapper/src/main/groovy/com/github/galcyurio/request/ForecastGribRequest.groovy
@@ -1,10 +1,8 @@
 package com.github.galcyurio.request
-
 /**
  * 검색구분, 검색어에 해당하는 초단기 실황 정보를 조회한다.
  *
  * @author galcyurio
  */
 class ForecastGribRequest {
-
 }
