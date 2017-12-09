cbf17ee9c69dfdf19dfa2bde6d4d39e351dfa2e2
galcyurio <galcyurio@gmail.com>
2017-12-09 21:21:57 +0900

    latitude and longitude to XY coordinate method and test added

--- vfiis-wrapper/src/main/groovy/com/github/galcyurio/commons/Util.groovy
+++ vfiis-wrapper/src/main/groovy/com/github/galcyurio/commons/Util.groovy
@@ -28,13 +28,58 @@ class Util {
         return sBaseDateTimeFormat.format(date)
     }
 
-    static Integer latitudeToNx(double latitude) {
-        // TODO: 위도를 x 좌표로 변환
-        return null
-    }
+    /**
+     * @param latitude 위도
+     * @param longitude 경도
+     * @return 위도 (lat) , 경도(lng)와 변환된 X(x), Y(y) 좌표가 포함된 map 객체를 반환한다.
+     */
+    static Map<String, Object> latLngToXY(double latitude, double longitude) {
+        double RE = 6371.00877 // 지구 반경(km)
+        double GRID = 5.0 // 격자 간격(km)
+        double SLAT1 = 30.0 // 투영 위도1(degree)
+        double SLAT2 = 60.0 // 투영 위도2(degree)
+        double OLON = 126.0 // 기준점 경도(degree)
+        double OLAT = 38.0 // 기준점 위도(degree)
+        double XO = 43 // 기준점 X좌표(GRID)
+        double YO = 136 // 기준점 Y좌표(GRID)
 
-    static Integer longitudeToNy(double longitude) {
-        // TODO: 경도를 y 좌표로 변환
-        return null
+        double degrad = Math.PI / 180.0
+
+        double re = RE / GRID
+        double slat1 = SLAT1 * degrad
+        double slat2 = SLAT2 * degrad
+        double olon = OLON * degrad
+        double olat = OLAT * degrad
+
+        double sn = Math.tan(Math.PI * 0.25 + slat2 * 0.5) / Math.tan(Math.PI * 0.25 + slat1 * 0.5)
+        sn = Math.log(Math.cos(slat1) / Math.cos(slat2)) / Math.log(sn)
+
+        double sf = Math.tan(Math.PI * 0.25 + slat1 * 0.5)
+        sf = Math.pow(sf, sn) * Math.cos(slat1) / sn
+
+        double ro = Math.tan(Math.PI * 0.25 + olat * 0.5)
+        ro = re * sf / Math.pow(ro, sn)
+
+        double ra = Math.tan(Math.PI * 0.25 + latitude * degrad * 0.5)
+        ra = re * sf / Math.pow(ra, sn)
+
+        double theta = longitude * degrad - olon
+        if (theta > Math.PI) {
+            theta -= 2.0 * Math.PI
+        }
+        if (theta < -Math.PI) {
+            theta += 2.0 * Math.PI
+        }
+        theta *= sn
+
+        int x = Math.floor(ra * Math.sin(theta) + XO + 0.5)
+        int y = Math.floor(ro - ra * Math.cos(theta) + YO + 0.5)
+
+        return [
+                lat: latitude,
+                lng: longitude,
+                x  : x,
+                y  : y
+        ]
     }
 }
--- vfiis-wrapper/src/test/groovy/commons/UtilTest.groovy
+++ vfiis-wrapper/src/test/groovy/commons/UtilTest.groovy
@@ -41,4 +41,68 @@ class UtilTest {
         assertThat(baseDateTime).isEqualTo("201710091530")
         println baseDateTime
     }
+
+    @Test
+    void latLngToXY_서울__mustReturnValidXY() throws Exception {
+        // 서울시 종로구
+        _latLngToXYTest(37.572975, 126.979367, 60, 127)
+    }
+
+    @Test
+    void latLngToXY_충청도__mustReturnValidXY() throws Exception {
+        // 충청남도 천안시 동남구 북면
+        _latLngToXYTest(36.832230, 127.267427, 65, 111)
+    }
+
+    @Test
+    void latLngToXY_전라도__mustReturnValidXY() throws Exception {
+        // 전라북도 무주군 적상면
+        _latLngToXYTest(35.945462, 127.659476, 72, 92)
+    }
+
+    @Test
+    void latLngToXY_경상도__mustReturnValidXY() throws Exception {
+        // 경상북도 포항시 북구 두호동
+        _latLngToXYTest(36.060197, 129.377114, 102, 95)
+    }
+
+    @Test
+    void latLngToXY_강원도__mustReturnValidXY() throws Exception {
+        // 강원도	 속초시 청호동
+        _latLngToXYTest(38.194192, 128.597619, 87, 141)
+    }
+
+    @Test
+    void latLngToXY_제주도__mustReturnValidXY() throws Exception {
+        // 제주특별자치도 서귀포시 송산동
+        _latLngToXYTest(33.226416, 126.567129, 53, 32)
+    }
+
+    @Test
+    void latLngToXY_독도__mustReturnValidXY() throws Exception {
+        // 경상북도 울릉군 독도
+        _latLngToXYTest(37.241768, 131.865288, 144, 123)
+    }
+
+    // 이어도 테스트 실패
+    // @Test
+    void latLngToXY_이어도__mustReturnValidXY() throws Exception {
+        // 이어도
+        _latLngToXYTest(33.074240, 126.210603, 28, 8)
+    }
+
+    // 약간의 오차 발생
+    // @Test
+    void latLngToXY_경상도_하동군_북천면__mustReturnValidXY() throws Exception {
+        // 경상남도 하동군 북천면
+        _latLngToXYTest(35.118642, 127.863090, 77, 74)
+    }
+
+    void _latLngToXYTest(double lat, double lng, int expectedX, int expectedY) {
+        def result = Util.latLngToXY(lat, lng)
+
+        println result
+        assertThat(result.get("x")).isEqualTo(expectedX)
+        assertThat(result.get("y")).isEqualTo(expectedY)
+    }
 }
